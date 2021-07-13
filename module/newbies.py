9328c9a6c556f7fba7385b0f505c0ff87f344ff3
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-07-13 11:58:16 +0100

    tidying code

--- module/newbies.py
+++ module/newbies.py
@@ -1,3 +1,12 @@
 def printplus(x):
     x2 = ("Guess my number between 10 and 20: ","Guess my 10s between 10 and 100: ")
     print(x2[x])
+
+def numpick(x):
+    guess = -1
+    while guess != x:
+       guess = int(input("Pick your number: "))
+       if guess != x:
+        print("WRONG!, try again")
+       else:
+        print("CORRECT!, you have won the game")
