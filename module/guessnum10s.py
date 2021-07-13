c66c5dc7856bf00b43221ce8d4559c66724306ea
Paul-Grubb <Paul.Grubb@nationwide.co.uk>
2021-07-13 12:00:02 +0100

    tidied guess num10s

--- module/guessnum10s.py
+++ module/guessnum10s.py
@@ -6,9 +6,4 @@ guess = -1
 
 newbies.printplus(1)
 
-while guess != x:
-    guess = int(input("Pick your number: "))
-    if guess != x:
-        print("WRONG!, try again")
-    else:
-        print("CORRECT!, you have won the game")
+newbies.numpick(x)
